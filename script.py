63711a969240bcb9131d4ed0ce7c124a9c418346
Jannes Teupen <jannesteupen@gmail.com>
2025-01-20 13:08:31 +0100

    extended declarations

--- script.py
+++ script.py
@@ -13,3 +13,27 @@ print(f"    Speicheradresse: {id(meine_liste)}")
 aendere_mutable_variable(meine_liste)
 print(f"Nach Funktionsaufruf (mutable): {meine_liste}")
 print(f"    Speicheradresse: {id(meine_liste)}")
+
+
+
+# Wie verändere ich eine Liste ohne das Original abzurufen?
+andere_liste = meine_liste
+print(f"\n\nAndere Liste : {andere_liste}")
+print(f"    Speicheradresse: {id(andere_liste)}")
+andere_liste.remove(100)
+print(f"Andere Liste : {andere_liste}")
+print(f"    Speicheradresse: {id(andere_liste)}")
+print(f"normale Liste: {meine_liste}")
+print(f"    Speicheradresse: {id(meine_liste)}")
+
+
+# -> indem ich eine Copy ziehe
+meine_liste.append(100)
+wieder_andere_liste = meine_liste.copy()
+print(f"\n\nWieder Andere Liste : {wieder_andere_liste}")
+print(f"    Speicheradresse: {id(wieder_andere_liste)}")
+wieder_andere_liste.remove(100)
+print(f"Wieder Andere Liste : {wieder_andere_liste}")
+print(f"    Speicheradresse: {id(wieder_andere_liste)}")
+print(f"normale Liste: {meine_liste}")
+print(f"    Speicheradresse: {id(meine_liste)}")
